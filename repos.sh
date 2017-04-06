1743043902f9ce25868697a5fc054fdbc0ce2b19
Buti <buti@nobuti.com>
2017-04-06 17:20:43 +0200

    Update windshaft branch to wotk with node 0.10

--- repos.sh
+++ repos.sh
@@ -3,4 +3,4 @@
 git clone --recursive https://github.com/cartodb/cartodb ../cartodb/cartodb
 git clone https://github.com/CartoDB/cartodb-postgresql.git ../cartodb/cartodb-postgresql
 git clone git://github.com/CartoDB/CartoDB-SQL-API.git ../cartodb/cartodb-sql-api
-git clone git://github.com/CartoDB/Windshaft-cartodb.git ../cartodb/windshaft-cartodb
+git clone -b v2.x git://github.com/CartoDB/Windshaft-cartodb.git ../cartodb/windshaft-cartodb
